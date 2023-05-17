7ed557b179ef9ca842d57e80e7b1c1843ac6a2a9
Shakil Ahmed <shakilahmedshuvo612@gmail.com>
2023-05-18 01:36:24 +0600

    google font add leto

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,16 +1,21 @@
-
+import { Link } from "react-router-dom";
 
 const Login = () => {
     return (
         <>
-            <div>
-                <h2 className="text-4xl font-bold">Login</h2>
+            <div className="mt-10">
+                <h2 className="text-4xl font-bold text-center">
+                    Login
+                </h2>
+                <p className="text-2xl font-bold text-center mt-2">
+                    Please Login!
+                </p>
             </div>
-            <div>
-                <div className="hero max-w-6xl mx-auto mt-20 ">
+            <div className="font-bold">
+                <div className="hero max-w-6xl mx-auto">
                     <div className="hero-content">
                         <div className="text-center lg:text-left">
-                            <img src="" alt="" />
+                            <img src="https://img.freepik.com/free-vector/global-data-security-personal-data-security-cyber-data-security-online-concept-illustration-internet-security-information-privacy-protection_1150-37373.jpg?w=740&t=st=1684351301~exp=1684351901~hmac=1360b442aeae7ada66feaaf7e313f926b3b3d6af866b6248fa40fe11ee76a7ce" alt="" />
                         </div>
                         <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
                             <div className="card-body">
@@ -28,11 +33,13 @@ const Login = () => {
                                             Password
                                         </span>
                                     </label>
-                                    <input type="text" placeholder="password" className="input input-bordered" />
+                                    <input type="password" placeholder="password" className="input input-bordered" />
                                     <label className="label">
-                                        <a href="#" className="label-text-alt link link-hover">
-                                            Forgot password?
-                                        </a>
+                                        <small className="text-neutral-700">
+                                            Don't Have an Account? New to Kids Toy World?
+                                            <br />
+                                            Please<Link className="font-extrabold text-sky-500" to='/registration'> Registration</Link>
+                                        </small>
                                     </label>
                                 </div>
                                 <div className="form-control mt-6">
